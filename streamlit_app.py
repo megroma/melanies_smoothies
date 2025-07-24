221ce78ec9472bee9e5bcc77cf45f3be55087626
Megan Romano <meganhromano@gmail.com>
2025-07-24 15:24:44 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ import requests
 # api_url = "https://fruityvice.com/api/fruit/all"
 import requests
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/all")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data= smoothiefroot_response.json(), use_container_width= True)
 
 # Write directly to the app
 st.title(f":cup_with_straw: Cutomize Your Smoothie!:cup_with_straw:")
